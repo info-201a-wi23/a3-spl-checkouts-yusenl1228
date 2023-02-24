4dbb5a47fcd40fbab879e73eabcd7172efdc0b1c
YusenLiang <yusen1228@gmail.com>
2023-02-24 12:40:26 -0800

    changed web link and minor changes to the r files

---
+++ summary.R
@@ -0,0 +1,7 @@
+# Load libraries
+library("dplyr")
+library("stringr")
+library("ggplot2")
+
+# Load the data
+spl_df <- read.csv("~/Desktop/INFO201/Local_resource/2017-2023-10-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
